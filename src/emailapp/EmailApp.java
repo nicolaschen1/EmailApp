86a3d0d7f5b16d96307063dbe54061116f589ded
Nicolas Chen <35080480+nicolaschen1@users.noreply.github.com>
2018-06-25 00:09:45 +0200

    Add Java source files.

---
+++ src/emailapp/EmailApp.java
@@ -0,0 +1,15 @@
+/************************
+ * Author: Nicolas Chen
+ * EmailApp
+ * Version 1.0
+*************************/
+
+package emailapp;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		Email em1 = new Email("Jack", "Wilson");
+		System.out.println(em1.showInfo());
+	}
+}
